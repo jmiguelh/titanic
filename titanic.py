e7444be1c5d873fb9f12fcbb2d973a7d320d65e6
Miguel Henriques <github@jmh.com.br>
2019-04-24 13:17:14 -0300

    Inclusão da exibição da árvore

--- titanic.py
+++ titanic.py
@@ -34,3 +34,15 @@ resultado = pd.DataFrame()
 resultado['PassengerId'] = teste2['PassengerId']
 resultado['Survived'] = tree.predict(teste2)
 resultado.to_csv('resultado.csv', index=False)
+
+from sklearn.externals.six import StringIO  
+from IPython.display import Image  
+from sklearn.tree import export_graphviz
+import pydotplus
+
+dot_data = StringIO()
+export_graphviz(tree, out_file=dot_data,  
+                filled=True, rounded=True,
+                special_characters=True)
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
+display(Image(graph.create_png()))
